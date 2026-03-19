1aa92ef7140cd648986ce264b95eff99636ea2df
Zamfree <84366286+Zamfree@users.noreply.github.com>
2026-03-20 03:22:30 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Finhalo Admin Portal",
+  description: "Finhalo Admin Portal",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
